d215e2e666d94227ce832fb435c5de19cace5e25
Your Name Herenaminamicapybara <32105854+GuillaumeHorikawa@users.noreply.github.com>
2020-09-08 04:06:50 -0400

    A3.1.0

---
+++ bonus/bonus.js
@@ -0,0 +1,11 @@
+document.getElementById('trigger').addEventListener('click', function(){
+    document.querySelector('body').classList.toggle('warpFilter');
+})
+
+
+document.getElementById('trigger').addEventListener('click', function(){
+    document.querySelector('#overlay').classList.toggle('toggle');
+    document.querySelector('#bonusHeader').classList.toggle('toggle');
+    document.querySelector('#spinningCircleTwo').classList.toggle('toggle')
+    document.querySelector('#trigger').classList.toggle('toggle');
+})
